2129a4e2bb8e7a211dd8331112e1ae81d3b2cd5b
salkorad <salko.radovovic@gmail.com>
2021-04-26 00:33:17 +0200

    Node Server

---
+++ src/controller.js
@@ -0,0 +1,15 @@
+const root = (req, res) => {
+    res.end('Home page is coming!');
+}
+
+const text = (req, res) => {
+    const param = req.params.param || 'Node';
+    res.end(`Hello ${param} Server!`);
+}
+
+const html = (req, res) => {
+    const param = req.params.param || 'Node';
+    res.end(`<h1>Hello ${param} Server!<h1>`);
+}
+
+export { root, text, html }
